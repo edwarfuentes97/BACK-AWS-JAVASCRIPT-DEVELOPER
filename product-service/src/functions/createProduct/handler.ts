17a644ad48c8d15c63d8005ba2f3798deb6cd87a
edward_fuentes <edward_fuentes@epam.com>
2022-07-27 16:06:08 -0500

    feat: task 4 implementation, solve task 4.3 (create products)

--- product-service/src/functions/createProduct/handler.ts
+++ product-service/src/functions/createProduct/handler.ts
@@ -3,33 +3,34 @@ import { middyfy } from '@libs/lambda';
 import cors from '@middy/http-cors';
 import {formatJSONResponse} from "@libs/api-gateway";
 import { v4 as UUID } from 'uuid';
-const { Pool } = require('pg');
+import {HTTPError} from "@functions/getProductsById/handler";
 
-const pool = new Pool ({
-  host: process.env.DATABASE_HOST,
+const ServerlessClient = require('serverless-postgres')
+
+const client = new ServerlessClient({
   user: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
+  host: process.env.DATABASE_HOST,
   database: process.env.DATABASE_NAME,
-  port: parseInt(process.env.DATABASE_PORT),
-  connectionTimeoutMillis: 10000
-})
-
+  password: process.env.DATABASE_PASSWORD,
+  port: process.env.DATABASE_PORT,
+  debug: true,
+  delayMs: 3000,
+});
 
 
 export const createProduct = middyfy( async (event)  => {
-  pool.connect((err, client, release) => {
-    if (err) {
-      return console.error('Error acquiring client', err.stack)
-    }
-    client.query('INSERT INTO products (id, title, description, price) VALUES($1, $2, $3, $4) RETURNING *',
-      [UUID(), event.body.title, event.body.description, event.body.price]
-      , (err, result) => {
-        release()
-        if (err) {
-          return console.error('Error executing query', err.stack)
-        }
-        return formatJSONResponse(result);
-      })
-  })
-
+  console.log(event.body)
+  let body = JSON.parse(event.body)
+  console.log('body2', body)
+  if (!event.body) {
+    throw new HTTPError(400, 'The data are not correctly sent ');
+  }
+  await client.connect();
+  const result = await client.query(
+    'INSERT INTO products (id, title, description, price) VALUES($1, $2, $3, $4) RETURNING *' , [
+      UUID(), body.title, body.description, body.price
+    ]);
+  await client.clean();
+  console.log(`body al final` , body)
+  return formatJSONResponse(result.rows)
 }).use(cors());
--- product-service/src/functions/createProduct/index.ts
+++ product-service/src/functions/createProduct/index.ts
@@ -2,7 +2,7 @@
 import { handlerPath } from '@libs/handler-resolver';
 
 export const createProduct = {
-  handler: `${handlerPath(__dirname)}/handler.getProductsList`,
+  handler: `${handlerPath(__dirname)}/handler.createProduct`,
   events: [
     {
       http: {
