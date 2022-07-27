626fe3822209e5500249f19c57abcba1f540abca
edward_fuentes <edward_fuentes@epam.com>
2022-07-27 15:28:57 -0500

    feat: task 4 implementation, solve task 4.3

--- product-service/src/functions/getProductsById/handler.ts
+++ product-service/src/functions/getProductsById/handler.ts
@@ -2,41 +2,31 @@ import { formatJSONResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
 // @ts-ignore
 import { default as products } from '../../shared/products.json';
-import cors from '@middy/http-cors';
 import {APIGatewayProxyEvent} from "aws-lambda";
+import cors from "@middy/http-cors";
+const ServerlessClient = require('serverless-postgres')
 
-
-const { Pool } = require('pg');
-
-const pool = new Pool ({
-  host: process.env.DATABASE_HOST,
+const client = new ServerlessClient({
   user: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
+  host: process.env.DATABASE_HOST,
   database: process.env.DATABASE_NAME,
-  port: parseInt(process.env.DATABASE_PORT),
-  connectionTimeoutMillis: 10000
-})
+  password: process.env.DATABASE_PASSWORD,
+  port: process.env.DATABASE_PORT,
+  debug: true,
+  delayMs: 3000,
+});
+
 
 export const GetProductsById = middyfy( async (event:APIGatewayProxyEvent)=> {
-
   const id = event.pathParameters.id;
   if (!id) {
     throw new HTTPError(400, 'Product id is not correct');
   }
-  pool.connect((err, client, release) => {
-    if (err) {
-      return console.error('Error acquiring client', err.stack)
-    }
-    client.query('SELECT * FROM products INNER JOIN stocks ON products.id = stocks.product_id WHERE id = $1', [id]
-      , (err, result) => {
-        release()
-        if (err) {
-          return console.error('Error executing query', err.stack)
-        }
-        console.log(result.rows)
-        return formatJSONResponse(result.rows);
-      })
-  })
+  await client.connect();
+  const result = await client.query(
+    'SELECT * FROM products INNER JOIN stocks ON products.id = stocks.product_id WHERE id = $1' , [id]);
+  await client.clean();
+  return formatJSONResponse(result.rows)
 }).use(cors());
 
 export class HTTPError extends Error {
--- product-service/src/functions/getProductsList/handler.ts
+++ product-service/src/functions/getProductsList/handler.ts
@@ -3,6 +3,7 @@ import { middyfy } from '@libs/lambda';
 // @ts-ignore
 import { default as products } from '../../shared/products.json';
 import {formatJSONResponse} from "@libs/api-gateway";
+import cors from "@middy/http-cors";
 
 const ServerlessClient = require('serverless-postgres')
 
@@ -24,4 +25,4 @@ export const getProductsList = middyfy( async ()  => {
     `SELECT * FROM products INNER JOIN stocks ON products.id = stocks.product_id`);
   await client.clean();
   return formatJSONResponse(result.rows)
-});
+}).use(cors());
